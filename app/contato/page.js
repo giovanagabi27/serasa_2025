47ea1084ed5e043d7ce4b9e153dd7e5b29c19371
giovanagabi27 <168477853+giovanagabi27@users.noreply.github.com>
2025-03-10 14:28:53 -0300

    atualização

---
+++ app/contato/page.js
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from "react";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+export default function Contato() {
+    const [nome, alteraNome] = useState("");
+    const [mensagem, alteraMensagem] = useState("");
+
+    function enviarMensagem(e) {
+        e.preventDefault();
+
+        if (nome === "" || mensagem === "") {
+            toast.error("Por favor, preencha todos os campos!");
+        } else {
+            toast.success("Mensagem enviada com sucesso!");
+            
+        }
+    }
+
+    return (
+        <div>
+            <h1>Página de contato</h1>
+            <p>Entre em contato conosco</p>
+            <hr />
+
+            <form onSubmit={enviarMensagem}>
+                <label>
+                    Digite seu nome:
+                    <input value={nome} onChange={(e) => alteraNome(e.target.value)} />
+                </label>
+                <br />
+
+                <label>
+                    Digite sua mensagem:
+                    <input value={mensagem} onChange={(e) => alteraMensagem(e.target.value)} />
+                </label>
+                <br />
+
+                <button>Enviar</button>
+            </form>
+            <ToastContainer />
+        </div>
+    );
+}
