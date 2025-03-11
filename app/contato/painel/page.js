35c77c1006c80f460aaad8207fcbd23e8d502ab9
giovanagabi27 <168477853+giovanagabi27@users.noreply.github.com>
2025-03-11 16:15:05 -0300

    atts

---
+++ app/contato/painel/page.js
@@ -0,0 +1,26 @@
+function Painel(){
+    return(
+        <div>
+
+        <h1>Painel administrativo</h1>
+
+        <label>
+            Digite o usuário:
+            <input/>
+        </label>
+        
+        <br/>
+
+        <label>
+            Digite a senha:
+            <input/>
+        </label>
+
+        <br/>
+
+        <button>Entrar</button>
+        
+
+        </div>
+    )
+}
